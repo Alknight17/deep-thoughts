b9fff72c531d176689605d4330aec188e4573384
Alknight17 <knightalex17@gmail.com>
2022-07-15 11:27:50 -0400

    forms

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import Auth from "../utils/auth";
+import { ADD_FRIEND } from "../utils/mutations";
+import ThoughtForm from '../components/ThoughtForm';
 
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
 const Profile = (props) => {
@@ -16,6 +18,7 @@ const Profile = (props) => {
   });
 
   const user = data?.me || data?.user || {};
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   // navigate to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -29,17 +32,34 @@ const Profile = (props) => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `{user.username}'s` : 'your'} profile
+          Viewing {userParam ? `{user.username}'s` : "your"} profile
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -58,6 +78,7 @@ const Profile = (props) => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
